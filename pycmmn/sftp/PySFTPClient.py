695c966b55cd6dcb28370a1566b53e822378f3b6
manki.baek <manki.baek@seculayer.com>
2023-05-30 15:18:20 +0900

    scp 라이브러리 오류로 인한 변경  : scp 라이브러리 제거  : 파일 전송 로직 변경

--- pycmmn/sftp/PySFTPClient.py
+++ pycmmn/sftp/PySFTPClient.py
@@ -3,10 +3,13 @@
 # e-mail : jin.kim@seculayer.com
 # Powered by Seculayer © 2021 Service Model Team, R&D Center.
 
+import stat
 import paramiko
-from scp import SCPClient, SCPException
+import os
+
 from pycmmn.crypto.AES256 import AES256
 from pycmmn.exceptions.PySFTPAuthException import PySFTPAuthException
+from pycmmn.utils.FileUtils import FileUtils
 
 
 class PySFTPClient(object):
@@ -44,21 +47,47 @@ class PySFTPClient(object):
         self.sftp.remove(file_path)
 
     def mkdir(self, dir_path):
-        self.sftp.mkdir(dir_path)
+        if not self.is_exist(dir_path):
+            self.sftp.mkdir(dir_path)
+
+    def is_dir(self, remote_path) -> bool:
+        fileattr = self.sftp.lstat(remote_path)
+        is_dir = False
+
+        if stat.S_ISDIR(fileattr.st_mode):
+            is_dir = True
+        elif stat.S_ISREG(fileattr.st_mode):
+            is_dir = False
+
+        return is_dir
 
     def scp_to_storage(self, local_path, remote_path):
-        try:
-            with SCPClient(self.transport) as scp:
-                scp.put(local_path, remote_path, recursive=True, preserve_times=True)
-        except SCPException:
-            raise SCPException
+        self._scp_send_recursive(local_path, remote_path)
+
+    def _scp_send_recursive(self, local_path, remote_path):
+        is_dir = os.path.isdir(local_path)
+        curr_dir_name = local_path.rsplit('/', 1)[1]
+
+        if is_dir:
+            self.mkdir(f"{remote_path}/{curr_dir_name}")
+            for file_name in os.listdir(local_path):
+                self._scp_send_recursive(f"{local_path}/{file_name}", f"{remote_path}/{curr_dir_name}")
+        else:
+            self.sftp.put(local_path, f"{remote_path}/{curr_dir_name}")
 
     def scp_from_storage(self, remote_path, local_path):
-        try:
-            with SCPClient(self.transport) as scp:
-                scp.get(remote_path, local_path, recursive=True)
-        except SCPException:
-            raise SCPException
+        self._scp_receive_recursive(remote_path, local_path)
+
+    def _scp_receive_recursive(self, remote_path, local_path):
+        is_dir = self.is_dir(remote_path)
+        curr_dir_name = remote_path.rsplit('/', 1)[1]
+
+        if is_dir:
+            FileUtils.mkdir(f"{local_path}/{curr_dir_name}")
+            for file_name in self.sftp.listdir(remote_path):
+                self._scp_receive_recursive(f"{remote_path}/{file_name}", f"{local_path}/{curr_dir_name}")
+        else:
+            self.sftp.get(remote_path, f"{local_path}/{curr_dir_name}")
 
 
 if __name__ == '__main__':
